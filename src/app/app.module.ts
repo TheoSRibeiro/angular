1974da34efac8ecea30f795910c3acd843c94610
theosribeiro <theosribeiro@gmail.com>
2023-04-08 16:00:58 -0300

    Navegacao - Router + icone + tootip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,12 @@ import { HeroDatailComponent } from './hero-detail/hero-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MessagesComponent } from './messages/messages.component';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -25,6 +29,10 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatToolbarModule,
     MatButtonModule,
     MatExpansionModule,
+    AppRoutingModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
